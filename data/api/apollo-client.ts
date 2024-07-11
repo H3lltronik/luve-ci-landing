d3502fe1417ee9f75658f6d2a5d7a9eab09f1da4
H3lltronik <esau.egs1@gmail.com>
2024-07-11 17:36:19 -0600

    chore: Update ApolloClient configuration with logger, authentication, and error handling

--- data/api/apollo-client.ts
+++ data/api/apollo-client.ts
@@ -1,4 +1,40 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, ApolloLink } from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+import { setContext } from '@apollo/client/link/context'
+
+const loggerLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((response) => {
+    return response
+  })
+})
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_URL
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    })
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`)
+  }
+})
+
+const link = ApolloLink.from([loggerLink, authLink, errorLink, httpLink])
 
 const cache = new InMemoryCache({
   typePolicies: ['Tag', 'BlogEntry', 'Author'].reduce(
@@ -8,12 +44,7 @@ const cache = new InMemoryCache({
 })
 
 const apolloClient = new ApolloClient({
-  link: new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_URL}`,
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`
-    }
-  }),
+  link,
   cache
 })
 
